e710b3f014d1fc85b65d4138fe956ac2453809aa
Kirill <k187m18733@gmail.com>
2026-05-14 12:48:45 +0300

    feat: add pre push and fix app test

--- class-components/src/__tests__/App.test.tsx
+++ class-components/src/__tests__/App.test.tsx
@@ -1,8 +1,7 @@
-import { describe, it, expect, vi, beforeEach, afterEach } from 'vitest';
-import { render, screen, waitFor, userEvent } from './utils/test-utils';
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
 import App from '../App';
 import * as api from '../services/api';
-import { mockPersons } from './utils/test-utils';
+import { mockPersons, render, screen, userEvent, waitFor } from './utils/test-utils';
 
 vi.mock('../services/api');
 
